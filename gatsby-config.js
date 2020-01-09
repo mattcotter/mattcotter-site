7304d276487a780565f8fbfd6979abd70711fd14
mattcotter <cotter.m@gmail.com>
2020-01-09 01:31:50 -0500

    update to package.json: added gh-pages package and deploy script

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,7 @@
 module.exports = {
-  pathPrefix: "/gatsby-react-bootstrap-starter",
   siteMetadata: {
     title: `Matt Cotter`,
-    description: `Matt Cotter is a web developer based out of Dayton, Ohio.`,
+    description: `Matt Cotter is a freelance web developer from Ohio.`,
     author: `Matt Cotter`,
   },
   plugins: [
