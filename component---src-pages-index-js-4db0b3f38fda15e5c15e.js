58f898fe7b9632cfe5c227b1a8fb14205cce1972
mattcotter <cotter.m@gmail.com>
2020-01-25 17:14:17 -0500

    Updates

---
+++ component---src-pages-index-js-4db0b3f38fda15e5c15e.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{RXBc:function(e,t,n){"use strict";n.r(t);var a=n("q1tI"),o=n.n(a),r=n("Bl7J"),c=n("vrFN");t.default=function(){return o.a.createElement(r.a,{pageInfo:{pageName:"index"}},o.a.createElement(c.a,{title:"Home",keywords:["Matt Cotter","Web Developer","Dayton, OH","Cincinnati, OH","Columbus, OH","Vue Developer","React Developer","Freelance"]}))}}}]);
+//# sourceMappingURL=component---src-pages-index-js-4db0b3f38fda15e5c15e.js.map
